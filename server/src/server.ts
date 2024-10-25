74975a786629f6db9401cd16c639d416b6645aba
Scott Saller <Scottmsaller@gmail.com>
2024-10-25 17:15:32 -0400

    Made changes inside of the server directory.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 dotenv.config();
 
 // Import the routes
@@ -10,9 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-
+app.use(express.static(path.join(__dirname, '../client/dist')))
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
